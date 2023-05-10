a9cec6226be87c3a1f65063773b368cb0de66498
angelgit <angelgugliuzzo@gmail.com>
2023-05-10 18:32:30 -0300

    Initial commit

---
+++ apisEnTs/src/main.ts
@@ -0,0 +1,20 @@
+//import { getUserById, getUsers } from './users/userService';
+
+//const listadoUsuarios = await getUsers();
+
+//console.log(listadoUsuarios.map((u) => u.addres.geo.lat));
+
+//const user = await getUserById(2);
+
+//console.log(user);
+
+
+import { getUserById, getUsers } from './users/userServices';
+
+const listadoUsuarios = await getUsers();
+
+console.log(listadoUsuarios.map((u) => u.addres.geo.lat));
+
+const user = await getUserById(2);
+
+console.log(user);
